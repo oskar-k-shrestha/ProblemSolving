02ac736eda7f28d089e529c3001e19a6f5f3225b
Oskar K. Shrestha <oscarstha@live.com>
2025-04-03 10:05:58 +0530

    add q

--- src/main/java/org/takeuforward/question/medium/RotateImage.java
+++ src/main/java/org/takeuforward/question/medium/RotateImage.java
@@ -10,10 +10,21 @@ public class RotateImage {
                 {9, 10, 11, 12},
                 {13, 14, 15, 16}
         };
-        System.out.println("Initial matrix : ");
+        System.out.println("Initial image : ");
         printMatrix(matrix);
-        System.out.println("Rotate matrix : ");
+        System.out.println("Rotate image : ");
         printMatrix(rotateImage(matrix));
+
+        matrix = new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        System.out.println("Initial image : ");
+        printMatrix(matrix);
+        System.out.println("Rotate image");
+        printMatrix(rotateImage1(matrix));
     }
 
     /**
@@ -31,6 +42,37 @@ public class RotateImage {
         }
 
         return result;
+    }
+
+    /**
+     * Time complexity : O(m * n)
+     * Space complexity : O(1)
+     */
+    private static int[][] rotateImage1(int[][] matrix) {
+
+        // transpose the matrix
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = row; column < matrix[row].length; column++) {
+                int temp = matrix[row][column];
+                matrix[row][column] = matrix[column][row];
+                matrix[column][row] = temp;
+            }
+        }
+
+        // reverse each row
+        for (int row = 0; row < matrix.length; row++) {
+            int start = 0;
+            int end = matrix[row].length - 1;
+            while (start <= end) {
+                int temp = matrix[row][start];
+                matrix[row][start] = matrix[row][end];
+                matrix[row][end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        return matrix;
     }
 
     private static void printMatrix(int[][] matrix) {
