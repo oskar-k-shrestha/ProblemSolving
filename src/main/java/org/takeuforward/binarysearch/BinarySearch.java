7c58f3485856428a7c63ef67606d9c75c0b8ce55
Oskar K. Shrestha <oscarstha@live.com>
2025-04-05 11:36:21 +0530

    add q

---
+++ src/main/java/org/takeuforward/binarysearch/BinarySearch.java
@@ -0,0 +1,60 @@
+package org.takeuforward.binarysearch;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 1;
+        System.out.println("Initial Array : " + Arrays.toString(arr));
+        System.out.println("Target " + target + " is at index " + binarySearch(arr, target));
+
+        System.out.println("Initial Array : " + Arrays.toString(arr));
+        System.out.println("Target " + target + " is at index " + binarySearchRecursive(arr, 0, arr.length - 1, target));
+    }
+
+    /**
+     * Time complexity : O(log(n))
+     * Space complexity : O(1)
+     */
+    private static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + (high - low)) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Time complexity : O(log(n))
+     * Space complexity : O(log(n))
+     */
+    private static int binarySearchRecursive(int[] arr, int low, int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + (high - low)) / 2;
+        if (arr[mid] == target) return mid;
+        if (arr[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+
+        return binarySearchRecursive(arr, low, high, target);
+    }
+}
