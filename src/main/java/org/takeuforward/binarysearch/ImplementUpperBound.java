01bd70ab7e22d79321ebcf56575056f356aa1184
Oskar K. Shrestha <oscarstha@live.com>
2025-04-05 17:00:55 +0530

    add q

--- src/main/java/org/takeuforward/binarysearch/ImplementUpperBound.java
+++ src/main/java/org/takeuforward/binarysearch/ImplementUpperBound.java
@@ -2,40 +2,41 @@ package org.takeuforward.binarysearch;
 
 import java.util.Arrays;
 
-public class ImplementLowerBound {
-
+public class ImplementUpperBound {
     public static void main(String[] args) {
         int[] arr = {1, 2, 2, 3};
         int target = 2;
         System.out.println("Initial array : " + Arrays.toString(arr));
-        System.out.println("Lower bound for target " + target + " is at index " + binarySearchLowerBound(arr, target));
+        System.out.println("Lower bound for target " + target + " is at index " + binarySearchUpperBound(arr, target));
         arr = new int[]{3,5,8,15,19};
         System.out.println("Initial array : " + Arrays.toString(arr));
-        System.out.println("Lower bound for target " + target + " is at index " + binarySearchLowerBound(arr, target));
+        System.out.println("Lower bound for target " + target + " is at index " + binarySearchUpperBound(arr, target));
         arr = new int[]{1,1,1,1,1};
         System.out.println("Initial array : " + Arrays.toString(arr));
-        System.out.println("Lower bound for target " + target + " is at index " + binarySearchLowerBound(arr, target));
+        System.out.println("Lower bound for target " + target + " is at index " + binarySearchUpperBound(arr, target));
+        arr = new int[]{3,5,8,9,15,19};
+        target = 9;
+        System.out.println("Initial array : " + Arrays.toString(arr));
+        System.out.println("Lower bound for target " + target + " is at index " + binarySearchUpperBound(arr, target));
     }
 
     /**
      * Time complexity : O(log(n))
      * Space complexity : O(1)
      */
-    private static int binarySearchLowerBound(int[] arr, int target) {
+    private static int binarySearchUpperBound(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
         int ans = arr.length;
 
         while (low <= high) {
             int mid = low + ((high - low) / 2);
-
-            if (arr[mid] >= target) {
-                // maybe an answer
+            if (arr[mid] > target) {
                 ans = mid;
-                // look on the left for smaller
+                // look on the left for smaller answer
                 high = mid - 1;
             } else {
-                // look on the right
+                // look on the right for bigger answer
                 low = mid + 1;
             }
         }
