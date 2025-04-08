e1b1d490c5b2f1eb0cd4b6ab044bdfafd61ab253
Oskar K. Shrestha <oscarstha@live.com>
2025-04-08 23:19:11 +0530

    add q

--- src/main/java/org/takeuforward/binarysearch/FindFirstAndLastPositionOfElement.java
+++ src/main/java/org/takeuforward/binarysearch/FindFirstAndLastPositionOfElement.java
@@ -8,6 +8,7 @@ public class FindFirstAndLastPositionOfElement {
         int target = 8;
         System.out.println("Initial array : " + Arrays.toString(arr));
         System.out.println("First and last position of element are " + Arrays.toString(position(arr, target)));
+        System.out.println("First and last position of element are " + Arrays.toString(position1(arr, target)));
     }
 
     /**
@@ -15,7 +16,7 @@ public class FindFirstAndLastPositionOfElement {
      * Space complexity : O(1)
      */
     private static int[] position(int[] arr, int target) {
-        int[] result = {-1,-1};
+        int[] result = {-1, -1};
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
@@ -27,5 +28,57 @@ public class FindFirstAndLastPositionOfElement {
         }
 
         return result;
+    }
+
+    /**
+     * Time complexity : O(2 * log(n)) 2 - pass
+     * Space complexity : O(1)
+     */
+    private static int[] position1(int[] arr, int target) {
+        int[] result = {-1, -1};
+
+        // look for lower bound
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (arr[mid] == target) {
+                // possible lower bound
+                if (result[0] == -1) {
+                    result[0] = mid;
+                } else {
+                    result[0] = Math.min(mid, result[0]);
+                }
+                // search in lower half for possible lower bound
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        // look for upper bound
+        low = 0;
+        high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (arr[mid] == target) {
+                // possible upper bound
+                result[1] = Math.max(mid, result[1]);
+
+                // search in upper half for possible upper bound
+                low = mid + 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+
     }
 }
