fda38ccee0ada6d9edf565b47192913e11b64bbb
Oskar K. Shrestha <oscarstha@live.com>
2025-04-28 23:02:27 +0530

    add q

--- src/main/java/org/takeuforward/binarysearch/onanswer/AllocateMinimumNumberOfPages.java
+++ src/main/java/org/takeuforward/binarysearch/onanswer/AllocateMinimumNumberOfPages.java
@@ -9,15 +9,17 @@ public class AllocateMinimumNumberOfPages {
         System.out.println("Initial arr : " + Arrays.toString(books));
         System.out.println("Number of students : " + m);
         System.out.println("Minimum maximum page a student gets : " + minNumberOfPage(books, m));
+        System.out.println("Minimum maximum page a student gets : " + minNumberOfPage1(books, m));
         books = new int[]{25, 46, 28, 49, 24};
         m = 4;
         System.out.println("Initial arr : " + Arrays.toString(books));
         System.out.println("Number of students : " + m);
         System.out.println("Minimum maximum page a student gets : " + minNumberOfPage(books, m));
+        System.out.println("Minimum maximum page a student gets : " + minNumberOfPage1(books, m));
     }
 
     /**
-     * Time complexity : O(n * r) here, r = sum(books) - books[max]
+     * Time complexity : O(n * r) here, r = sum of books - max(book pages)
      * Space complexity : O(1)
      * Trick : Identify your probable answer range.
      * 1) For a given set of books, what is the minimum page distribution?
@@ -39,12 +41,12 @@ public class AllocateMinimumNumberOfPages {
         System.out.println("max possible pages : " + high);
 
         for (int limit = low; limit <= high; limit++) {
-            if (canAllocate(books, m, limit)) return limit;
+            if (canAllocate(books, m, limit) == 0) return limit;
         }
         return -1;
     }
 
-    private static boolean canAllocate(int[] books, int m, int limit) {
+    private static int canAllocate(int[] books, int m, int limit) {
         int studentsAllocated = 1;
         int pagesAllocated = books[0];
         for (int i = 1; i < books.length; i++) {
@@ -56,9 +58,40 @@ public class AllocateMinimumNumberOfPages {
                 studentsAllocated++;
                 pagesAllocated = books[i];
             }
-            if (studentsAllocated > m) return false;
+            if (studentsAllocated > m) return 1;
         }
-        return studentsAllocated == m;
+        if (studentsAllocated < m) return -1;
+        return 0;
+    }
+
+    /**
+     * Time complexity : O(n * log(r)) here, r = sum of books - max(book pages)
+     * Space complexity : O(1)
+     */
+    private static int minNumberOfPage1(int[] books, int m) {
+        // we need to allocate at least one book to a student
+        if (books.length < m) return -1;
+
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+        for (int pages : books) {
+            if (pages > low) {
+                low = pages;
+            }
+            high += pages;
+        }
+
+        while (low <= high) {
+            int limit = low + ((high - low) / 2);
+            if (canAllocate(books, m, limit) <= 0) {
+                // look for minimum page allocation
+                // move left
+                high = limit - 1;
+            } else {
+                low = limit + 1;
+            }
+        }
+        return low;
     }
 
 }
