9e5864b2f34af6d38bbe308d1261e40127527003
Oskar K. Shrestha <oscarstha@live.com>
2025-05-03 13:19:21 +0530

    add q

--- src/main/java/org/takeuforward/binarysearch/onanswer/MinimiseMaximumDistanceBetweenGasStation.java
+++ src/main/java/org/takeuforward/binarysearch/onanswer/MinimiseMaximumDistanceBetweenGasStation.java
@@ -10,18 +10,23 @@ public class MinimiseMaximumDistanceBetweenGasStation {
         System.out.println("Gas stations to add : " + k);
         System.out.println("Minimum maximum distance between gas station : " + addStations(arr, k));
         System.out.println("Minimum maximum distance between gas station : " + addStations1(arr, k));
+        System.out.println("Minimum maximum distance between gas station : " + addStation2(arr, k));
         arr = new int[]{1,2,3,4,5,6,7,8,9,10};
         k = 1;
         System.out.println("Initial arr : " + Arrays.toString(arr));
         System.out.println("Gas stations to add : " + k);
         System.out.println("Minimum maximum distance between gas station : " + addStations(arr, k));
         System.out.println("Minimum maximum distance between gas station : " + addStations1(arr, k));
+        System.out.println("Minimum maximum distance between gas station : " + addStation2(arr, k));
         arr = new int[]{1,7};
         k = 2;
         System.out.println("Initial arr : " + Arrays.toString(arr));
         System.out.println("Gas stations to add : " + k);
         System.out.println("Minimum maximum distance between gas station : " + addStations(arr, k));
         System.out.println("Minimum maximum distance between gas station : " + addStations1(arr, k));
+        System.out.println("Minimum maximum distance between gas station : " + addStation2(arr, k));
+
+
     }
 
 
@@ -89,6 +94,42 @@ public class MinimiseMaximumDistanceBetweenGasStation {
         }
 
         return maxAnswer;
+    }
+
+    /**
+     * Time complexity : O(n + (n * log(maxDist)) here, maxDist is the max distance between two adjacent gas station
+     * Space complexity : O(1)
+     */
+    private static double addStation2(int[] arr, int k) {
+        double low = 0;
+        double high = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (high < arr[i + 1] - arr[i]) {
+                high = arr[i + 1] - arr[i];
+            }
+        }
+
+        while (high - low > 1e-6) {
+            double mid = low + ((high - low) / 2.0);
+            if (numberOfGasStationRequired(mid, arr) > k) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return high;
+    }
+
+    private static int numberOfGasStationRequired(double dist, int[] arr) {
+        int cnt = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int numberInBetween = (int) ((arr[i] - arr[i - 1]) / dist);
+            if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+                numberInBetween--;
+            }
+            cnt += numberInBetween;
+        }
+        return cnt;
     }
 
 
