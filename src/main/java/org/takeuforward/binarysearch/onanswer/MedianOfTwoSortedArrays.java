f56e54d8009cb91b3505d4d56d951345ba304ee2
Oskar K. Shrestha <oscarstha@live.com>
2025-05-04 22:22:34 +0530

    add q

--- src/main/java/org/takeuforward/binarysearch/onanswer/MedianOfTwoSortedArrays.java
+++ src/main/java/org/takeuforward/binarysearch/onanswer/MedianOfTwoSortedArrays.java
@@ -16,6 +16,7 @@ public class MedianOfTwoSortedArrays {
         System.out.println("Initial arr2 : " + Arrays.toString(arr2));
         System.out.println("Median value : " + median(arr1, arr2));
         System.out.println("Median value : " + median1(arr1, arr2));
+        System.out.println("Median value : " + median2(arr1, arr2));
 
         arr1 = new int[]{1, 2};
         arr2 = new int[]{3, 4};
@@ -23,13 +24,15 @@ public class MedianOfTwoSortedArrays {
         System.out.println("Initial arr2 : " + Arrays.toString(arr2));
         System.out.println("Median value : " + median(arr1, arr2));
         System.out.println("Median value : " + median1(arr1, arr2));
+        System.out.println("Median value : " + median2(arr1, arr2));
 
-        arr1 = new int[]{1, 3};
-        arr2 = new int[]{2};
+        arr1 = new int[]{2};
+        arr2 = new int[]{};
         System.out.println("Initial arr1 : " + Arrays.toString(arr1));
         System.out.println("Initial arr2 : " + Arrays.toString(arr2));
         System.out.println("Median value : " + median(arr1, arr2));
         System.out.println("Median value : " + median1(arr1, arr2));
+        System.out.println("Median value : " + median2(arr1, arr2));
 
         long endTime = System.nanoTime();
         long endMemory = runtime.totalMemory() - runtime.freeMemory();
@@ -146,5 +149,43 @@ public class MedianOfTwoSortedArrays {
         }
 
         return (index1Element + index2Element) / 2.0;
+    }
+
+    /**
+     * Time complexity : O(log(min(n1,n2))
+     * Space complexity : O(1)
+     */
+    private static double median2(int[] arr1, int[] arr2) {
+        int n1 = arr1.length, n2 = arr2.length;
+        // swap array if arr1 is larger
+        if (n1 > n2) return median2(arr2, arr1);
+
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2; // length of left half
+
+        int low = 0;
+        int high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            // calculate l1, l2, r1, r2
+            int l1 = (mid1 > 0) ? arr1[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? arr2[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? arr1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? arr2[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) {
+                    return Math.max(l1, l2);
+                } else {
+                    return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+                }
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
+        }
+        return -1;
     }
 }
