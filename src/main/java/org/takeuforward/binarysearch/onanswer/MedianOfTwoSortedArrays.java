9096a3c7bdfc684cb31ef0a1d05ab6f9d7a04a70
Oskar K. Shrestha <oscarstha@live.com>
2025-05-04 11:25:20 +0530

    add q

--- src/main/java/org/takeuforward/binarysearch/onanswer/MedianOfTwoSortedArrays.java
+++ src/main/java/org/takeuforward/binarysearch/onanswer/MedianOfTwoSortedArrays.java
@@ -4,23 +4,41 @@ import java.util.Arrays;
 
 public class MedianOfTwoSortedArrays {
     public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc(); // hint to GC
+
+        long startMemory = runtime.totalMemory() - runtime.freeMemory();
+        long startTime = System.nanoTime();
+
         int[] arr1 = {2, 4, 6};
         int[] arr2 = {1, 3, 5};
         System.out.println("Initial arr1 : " + Arrays.toString(arr1));
         System.out.println("Initial arr2 : " + Arrays.toString(arr2));
         System.out.println("Median value : " + median(arr1, arr2));
+        System.out.println("Median value : " + median1(arr1, arr2));
 
         arr1 = new int[]{1, 2};
         arr2 = new int[]{3, 4};
         System.out.println("Initial arr1 : " + Arrays.toString(arr1));
         System.out.println("Initial arr2 : " + Arrays.toString(arr2));
         System.out.println("Median value : " + median(arr1, arr2));
+        System.out.println("Median value : " + median1(arr1, arr2));
 
         arr1 = new int[]{1, 3};
         arr2 = new int[]{2};
         System.out.println("Initial arr1 : " + Arrays.toString(arr1));
         System.out.println("Initial arr2 : " + Arrays.toString(arr2));
         System.out.println("Median value : " + median(arr1, arr2));
+        System.out.println("Median value : " + median1(arr1, arr2));
+
+        long endTime = System.nanoTime();
+        long endMemory = runtime.totalMemory() - runtime.freeMemory();
+
+        long timeElapsed = endTime - startTime;
+        long memoryUsed = endMemory - startMemory;
+
+        System.out.println("Execution Time: " + (timeElapsed / 1_000_000.0) + " ms");
+        System.out.println("Memory Used: " + (memoryUsed / 1024.0 / 1024.0) + " MB");
     }
 
     /**
@@ -63,5 +81,70 @@ public class MedianOfTwoSortedArrays {
         } else {
             return merged[n / 2];
         }
+    }
+
+    /**
+     * Time complexity : O(m + n)
+     * Space complexity : O(1)
+     */
+    private static double median1(int[] arr1, int[] arr2) {
+        int n = arr1.length + arr2.length;
+
+        int indexArr1 = 0;
+        int indexArr2 = 0;
+
+        int index1 = (n / 2) - 1;
+        int index2 = n / 2;
+        int index1Element = -1;
+        int index2Element = -1;
+
+        int curr = 0;
+        while (indexArr1 < arr1.length && indexArr2 < arr2.length) {
+            if (arr1[indexArr1] <= arr2[indexArr2]) {
+                if (curr == index1) {
+                    index1Element = arr1[indexArr1];
+                } else if (curr == index2) {
+                    index2Element = arr1[indexArr1];
+                }
+                curr++;
+                indexArr1++;
+            } else {
+                if (curr == index1) {
+                    index1Element = arr2[indexArr2];
+                } else if (curr == index2) {
+                    index2Element = arr2[indexArr2];
+                }
+                curr++;
+                indexArr2++;
+            }
+        }
+
+        // check if arr1 is not exhausted
+        while (indexArr1 < arr1.length) {
+            if (curr == index1) {
+                index1Element = arr1[indexArr1];
+            } else if (curr == index2) {
+                index2Element = arr1[indexArr1];
+            }
+            curr++;
+            indexArr1++;
+        }
+
+        // check if arr2 is not exhausted
+        while (indexArr2 < arr2.length) {
+            if (curr == index1) {
+                index1Element = arr2[indexArr2];
+            } else if (curr == index2) {
+                index2Element = arr2[indexArr2];
+            }
+            curr++;
+            indexArr2++;
+        }
+
+        if (n % 2 == 1) {
+            return index2Element;
+        }
+
+        return (index1Element + index2Element) / 2.0;
     }
 }
